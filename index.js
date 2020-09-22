f4c61d084ee54bc75d47c5b504c2da39f22ca5e5
YoussefHeshamAlTaher <heshamyuosif@gmail.com>
2020-09-22 15:49:11 +0200

    path changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const isAuth = require('./middleware/is-auth')
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname,'client/build/index.html'));
+      res.sendFile(path.join(__dirname,'client','build','index.html'));
     });
   }
 app.use(express.static("public"));
